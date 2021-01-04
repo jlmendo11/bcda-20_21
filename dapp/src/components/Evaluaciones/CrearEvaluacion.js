088c417fdbf23b250bafd619382b41d9ce871c00
jlmendo11 <jlmendo11>
2021-01-04 19:00:33 +0100

    crearEvaluacion refactored

--- dapp/src/components/Evaluaciones/CrearEvaluacion.js
+++ dapp/src/components/Evaluaciones/CrearEvaluacion.js
@@ -1,11 +1,9 @@
 import {newContextComponents} from "@drizzle/react-components";
 const {ContractData, ContractForm} = newContextComponents;
 
-const CrearEvaluacion = ({drizzle, drizzleState}) => <CrearEval  drizzle={drizzle} drizzleState={drizzleState} />
+const CrearEvaluacion = ({drizzle, drizzleState}) => 
 
-
-
-const CrearEval = ({drizzle, drizzleState}) => <article className="AppMisDatos">
+<article className="AppMisDatos">
     <h3>Crear Evaluacion</h3>
     <ContractData drizzle={drizzle} drizzleState={drizzleState}
                   contract={"Asignatura"} method={"profesor"} methodArgs={[]}
@@ -17,7 +15,7 @@ const CrearEval = ({drizzle, drizzleState}) => <article className="AppMisDatos">
                                            contract="Asignatura" method="creaEvaluacion"
                                            labels={["Nombre Evaluacion", "Fecha(Segundos desde 1970)",
                                                "Puntos de la Nota Final"]}
-                      />
+                            />
                   }}
     />
 </article>
